85fa9efa86374f758d94a61949a722fe6e4bfb41
233703rahul <233703@ust-global.com>
2022-10-10 17:11:31 +0530

    Initial commit

---
+++ src/Practice/Least.java
@@ -0,0 +1,38 @@
+package Practice;
+import java.util.*;
+
+public class Least {
+
+
+
+	   public static void main(String[] args) {
+	        
+	        Scanner s=new Scanner(System.in);
+	        System.out.println("enter the number of items:");
+	        int n = s.nextInt(); s.nextLine();
+	        String input1[][] = new String[n][3];
+	        
+	        for(int i=0;i<n;i++){
+	          input1[i] = s.nextLine().split(",");
+	          }
+	          
+	          int discount[] = new int[n];
+	          for(int i=0;i<n;i++){
+	            discount[i] = (Integer.parseInt(input1[i][1])*Integer.parseInt(input1[i][2])/100);
+	          }
+	          
+	          int min=9999;
+	          for(int i=0;i<n;i++){
+	            if(min>discount[i])min=discount[i];
+	          }
+	          
+	          for(int i=0;i<n;i++){
+	            if(min==discount[i])
+	                System.out.print(input1[i][0]+" ");
+	          }
+	        
+	    }
+
+
+
+	   }
